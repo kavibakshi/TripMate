4fbf39fe0551f9495b428d04cbb507007045d664
SahirSood <sahirsood@gmail.com>
2023-10-06 13:02:41 -0700

    Second page called map

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #get 'home/index'
+  get 'home/map'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
