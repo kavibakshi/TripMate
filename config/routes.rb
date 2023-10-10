8faba1c4f0c7fcc4f5889297210de99cbdd052bd
SahirSood <sahirsood@gmail.com>
2023-10-09 21:03:29 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  devise_for :accounts
-  #get 'home/index'
-  get 'home/map'
+  get "about-page", to:"about#index", as: :about
 
   get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
 
-<<<<<<< Updated upstream
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-=======
   get '/admin/dashboard', to: 'admin_dashboard#index', as: 'admin_dashboard'
 
 
   delete "logout", to: "sessions#destroy"
 
   root to: "home#index"
->>>>>>> Stashed changes
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
+
