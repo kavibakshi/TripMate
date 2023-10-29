79b6892dfc7db045ced364f8d073a8c6ec667275
vallescas <122315135+vallescas@users.noreply.github.com>
2023-10-28 20:09:10 -0700

    navbar list + notes additions
    - added a button for list features in the navigation bar
    - added a button for notes features in the navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   resources :lists
-  get "about-page", to:"about#index", as: :about
+  get "about", to:"about#index", as: :about
+  get "lists", to:"lists#index"
+  get "notes", to: "notes#index", as: :notes
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
