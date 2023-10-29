1d57560173f7dcdbcc4930835d6b9ae4c9436b14
vallescas <122315135+vallescas@users.noreply.github.com>
2023-10-28 21:47:53 -0700

    added create note function
    - users can now create notes under url/notes/new

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_233636) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_032238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string "description"
     t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
