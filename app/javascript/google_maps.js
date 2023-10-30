3906e23b6f746e27bf0ec43d14f33d835801255c
SahirSood <sahirsood@gmail.com>
2023-10-29 17:48:34 -0700

    Began implementation of leaflet

---
+++ app/javascript/google_maps.js
@@ -0,0 +1,3 @@
+<script>
+    var map = L.map('map').setView([51.505, -0.09], 13);
+</script>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get 'map/index'
   resources :lists
   get "about-page", to:"about#index", as: :about
 
