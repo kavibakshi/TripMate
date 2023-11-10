7609fb7b089d4f0d8e4dc1e2cc08e80681dc707c
Celia Fan <celia.f2000@gmail.com>
2023-11-09 17:48:56 -0800

    Added weather feature to navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get "notes", to: "notes#index"
   get "expense_trackers", to: "expense_trackers#index"
   get "map", to: "map#index"
-  get 'weather/index'
+  get 'weather', to:"weather#index"
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
